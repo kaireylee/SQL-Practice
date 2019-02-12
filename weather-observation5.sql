d52f11aae48e4dbae457f6af28250946ec3f6090
xants <kairey@gmail.com>
2019-02-11 19:59:15 -0800

    Hacker rank solutions

---
+++ weather-observation5.sql
@@ -0,0 +1,15 @@
+/*Query the two cities in STATION with the shortest and longest CITY names,
+as well as their respective lengths (i.e.: number of characters in the name).
+If there is more than one smallest or largest city, choose the one that comes
+first when ordered alphabetically.*/
+
+
+SELECT CITY, CHAR_LENGTH(CITY) FROM STATION
+WHERE CHAR_LENGTH(CITY) = (SELECT MAX(CHAR_LENGTH(CITY)) FROM STATION)
+ORDER BY CITY
+LIMIT 1;
+
+SELECT CITY, CHAR_LENGTH(CITY) FROM STATION
+WHERE CHAR_LENGTH(CITY) = (SELECT MIN(CHAR_LENGTH(CITY)) FROM STATION)
+ORDER BY CITY
+LIMIT 1;
