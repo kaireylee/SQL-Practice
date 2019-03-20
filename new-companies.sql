da4f259e22cf0a4d8809ce5f31d1fc9ab05e41f8
xants <kairey@gmail.com>
2019-03-20 13:12:13 -0700

    removed unecessary conditions as all managers are linked to employee table

--- new-companies.sql
+++ new-companies.sql
@@ -6,14 +6,11 @@ Order your output by ascending company_code.*/
 select 
     c.company_code, 
     c.founder, 
-    count(distinct lm.lead_manager_code), 
-    count(distinct sm.senior_manager_code), 
-    count(distinct m.manager_code),
+    count(distinct e.lead_manager_code), 
+    count(distinct e.senior_manager_code), 
+    count(distinct e.manager_code),
     count(distinct e.employee_code) 
-from Company c, Lead_Manager lm, Senior_Manager sm, Manager m, Employee e 
-where c.company_code = lm.company_code 
-    and lm.lead_manager_code = sm.lead_manager_code 
-    and sm.senior_manager_code = m.senior_manager_code 
-    and m.manager_code = e.manager_code 
+from Company c
+    inner join employee e on e.company_code = c.company_code
 group by c.company_code order by c.company_code;
 
