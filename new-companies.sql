d174149b5c1d2e2a37518c4447df45cd5b616817
xants <kairey@gmail.com>
2019-03-23 23:16:29 -0700

    Update new-companies.sql
    fixed groupby

--- new-companies.sql
+++ new-companies.sql
@@ -12,5 +12,5 @@ select
     count(distinct e.employee_code) 
 from Company c
     inner join employee e on e.company_code = c.company_code
-group by c.company_code order by c.company_code;
+group by c.company_code, c.founder order by c.company_code;
 
